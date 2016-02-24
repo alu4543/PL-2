d026e88528a29421992df8dda07951a437b216d0
mohammed <simotenerife@hotmail.com>
2016-02-24 15:45:51 +0000

    añadir unidad kelvin [opcionl] #1
    Adaptar el codigo para aceptar la unidad Kelvin [opcionl]

--- temperature.js
+++ temperature.js
@@ -1,6 +1,6 @@
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 function calculate() {
-  var result;
+  var result1, result2;
   var temp = original.value;
   var regexp = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+(?:\.\d+)?)?)\s*(º?(f(arenheit)?|c(elsius)?)|(k(elvin)?))\s*$/i;
 
@@ -16,27 +16,52 @@ function calculate() {
 
     if (type.substr(0) == 'c') {
       //Celsius	to Fahrenheit	ºF = ºC x 1.8 + 32
-      result = (num * 1.8)+32;
-      result = result.toFixed(1)+" Farenheit"
+      result1 = (num * 1.8)+32;
+      result1 = result1.toFixed(1)+" Farenheit"
 
+      //Celsius to	Kelvin	K = ºC + 273.15
+      result2 = num + 273.15;
+      result2 = result2.toFixed(1)+" Kelvin"
     }
-
-    else {
+    else if (type.substr(0) == 'f'){
       //Fahrenheit to	Celsius	ºC = ( ºF - 32) / 1.8
-      result = (num - 32)*5/9;
-      result = result.toFixed(1)+" Celsius"
+      result1 = (num - 32)*5/9;
+      result1 = result1.toFixed(1)+" Celsius"
 
+      //Fahrenheit to	Kelvin	K = ( ºF + 459.67) / 1.8
+      result2 = (num + 459.67) / 1.8;
+      result2 = result2.toFixed(1)+" Kelvin"
     }
-    converted.innerHTML = result;
+    else {
+      //Kelvin	Fahrenheit	ºF = K × 1.8 - 459.67
+      result1 = (num * 1.8) - 459.67;
+      result1 = result1.toFixed(1)+" Farenheit"
 
+      //Kelvin to	Celsius	ºC = K – 273.15
+      result2 = (num - 273.15);
+      result2 = result2.toFixed(1)+" Celsius"
+    }
+    converted.innerHTML = result1;
+    converted2.innerHTML = result2;
   }
   else {
     converted.innerHTML = "ERROR! Try something like '0celsius, 32ºF, -45K, 3.4e2 c, -2.3e-4kelvin,..";
+    converted2.innerHTML = "ERROR! Try something like '0celsius, 32ºF, -45K, 3.4e2 c, -2.3e-4kelvin,..";
   }
 }
 
 
-// numeros exponenciales(Transformar): http://tip.dis.ulpgc.es/numeros-texto/default.aspx
+
+/*
+Fahrenheit	Celsius	ºC = ( ºF - 32) / 1.8
+Celsius	Fahrenheit	ºF = ºC x 1.8 + 32
+Fahrenheit	Kelvin	K = ( ºF + 459.67) / 1.8
+Kelvin	Fahrenheit	ºF = K × 1.8 - 459.67
+Celsius	Kelvin	K = ºC + 273.15
+Kelvin	Celsius	ºC = K – 273.15
+*/
+
+// umeros exponenciales(Transformar): http://tip.dis.ulpgc.es/numeros-texto/default.aspx
 // Expresiones regulares(Graficamente) : http://regexper.com
 // Expresiones regulares : http://www.arumeinformatica.es/blog/expresiones-regulares-conceptos-basicos/
 //* : Busca el caracter precedente 0 (cero) o más veces.
